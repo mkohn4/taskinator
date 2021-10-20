5ff2402781b611602e28518ca634d284df7585df
maxkohn <kohn.max@gmail.com>
2021-10-20 08:51:06 -0400

    added buttons in html and js to create counter and unique buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 var formEl = document.querySelector('#task-form');
 //set task container as a variable
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+//declare taskIdCounter
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
     //prevent default web browser refresh on submit
@@ -29,11 +31,16 @@ var taskFormHandler = function(event) {
    
 }
 
+
+
 var createTaskEl = function(taskDataObj) {
     //set variable =  list item creation
     var listItemEl = document.createElement("li");
     //add css attribute to list item
     listItemEl.className = "task-item";
+
+    //add task id as custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
 
     //create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
@@ -44,10 +51,64 @@ var createTaskEl = function(taskDataObj) {
     
     //append div with task name and type to the list items
     listItemEl.appendChild(taskInfoEl);
-    
+
+    //run createTaskActions function for the current counted element to create edit, delete, and select btns
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    //append edit, delete, select to each li task
+    listItemEl.appendChild(taskActionsEl);
+   
     //add entire list item into the task container
     tasksToDoEl.appendChild(listItemEl);
-}
+
+    //increase task counter
+    taskIdCounter++;
+};
+
+var createTaskActions = function(taskId) {
+    //create div with class task-actions
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    //create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+    actionContainerEl.appendChild(editButtonEl);
+
+    //Create Delete Button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    //create select elements and add to nox
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+    actionContainerEl.appendChild(statusSelectEl);
+    //declare var for statuses
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (var i = 0; i < statusChoices.length; i++) {
+        //create option element
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+
+        //append to select
+        statusSelectEl.appendChild(statusOptionEl);
+    };
+
+
+
+    return actionContainerEl;
+
+};
+
+
 
 //listen for button click and create new list item task
 formEl.addEventListener("submit", taskFormHandler);
